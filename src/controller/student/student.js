a940b43961625d47644c9d1f63d250912b254e87
Trong Nhan <trongnhannguyen94@gmail.com>
2020-07-13 13:48:21 +0700

    13/07/2020

--- src/controller/student/student.js
+++ src/controller/student/student.js
@@ -9,42 +9,19 @@ var user;
 const addSubject = async (req, res, next) => {
   try {
     const isMobile = req.header("Accept").includes("application/json");
-    if(isMobile){
-      const {studentID} = req.query;
-      const user = await User.findById(studentID);
-      const scheduleID = user.schedule;
-      const transcriptID = user.transcript;
-      const subject = req.body;
-      var check = false;
-      const schedule = await Schedule.findById(scheduleID).populate(
-        "subjects.subject"
-      );
-      const transcript = await Transcript.findById(transcriptID).populate(
-        "subjects.subject"
-      );
-      const transcriptList = [...transcript.subjects];
-      transcriptList.forEach((item) => {
-        if (item.subject._id.toString() === subject.subject) {
-          return (check = true);
-        }
-      });
-      if (check) return res.status(201).send("You have passed or studying this subject");
-  
-      const subjectTranscript = {
-        subject: subject.subject,
-        scores: 0,
-        status: "Learning",
-      };
-      transcript.subjects.push(subjectTranscript);
-      schedule.subjects.push(subject);
-      await schedule.save();
-      await transcript.save();
-      return res.status(200).send("Registration successfully");
-    }
-    const scheduleID = user.schedule;
-    const transcriptID = user.transcript;
     const subject = req.body;
+    var scheduleID, transcriptID;
     var check = false;
+    if (user) {
+      scheduleID = user.schedule;
+      transcriptID = user.transcript;
+    } else {
+      const { studentID } = req.query;
+      const user = await User.findById(studentID);
+      scheduleID = user.schedule;
+      transcriptID = user.transcript;
+    }
+   
     const schedule = await Schedule.findById(scheduleID).populate(
       "subjects.subject"
     );
@@ -57,7 +34,10 @@ const addSubject = async (req, res, next) => {
         return (check = true);
       }
     });
-    if (check) return res.send("You have passed or studying this subject");
+    if (check){
+      if(isMobile) return res.status(201).send("You have passed or studying this subject");
+      return res.send("You have passed or studying this subject")
+    };
 
     const subjectTranscript = {
       subject: subject.subject,
@@ -68,6 +48,7 @@ const addSubject = async (req, res, next) => {
     schedule.subjects.push(subject);
     await schedule.save();
     await transcript.save();
+    if(isMobile) return res.status(200).send("Registration successfully you can check your schedule now");
     return res.redirect("/student/schedule");
   } catch (error) {
     return res.send(error.message);
@@ -76,33 +57,30 @@ const addSubject = async (req, res, next) => {
 // Change Password
 const changePassword = async (req, res, next) => {
   try {
+  
     const { currentPass, password } = req.body;
     const isMobile = req.header("Accept").includes("application/json");
-    if (isMobile) {
-      const {studentID} = req.query;
-      const student = await User.findById(studentID);
-      const checkPass = await bcrypt.compare(currentPass, student.password);
-      if (checkPass) {
-        const salt = await bcrypt.genSalt(10);
-        const passHashed = await bcrypt.hash(password, salt);
-        student.password = passHashed;
-        await student.save();
-        return res.status(200).send("Update successfully");
-      }
-      return  res.status(201).send("Your current password incorrect");
-    };
+    var studentID, passwordUser;
+    if(user){
+      studentID = user._id;
+      passwordUser =user.password;
+    }else{
+      studentID = req.query.studentID;
+      const user = await User.findById(studentID);
+      passwordUser = user.password;
+    }
     
-
-    const studentID = user._id;
     const student = await User.findById(studentID);
-    const checkPass = await bcrypt.compare(currentPass, user.password);
+    const checkPass = await bcrypt.compare(currentPass,passwordUser );
     if (checkPass) {
       const salt = await bcrypt.genSalt(10);
       const passHashed = await bcrypt.hash(password, salt);
       student.password = passHashed;
       await student.save();
-     return res.redirect("/student/profile");
+      if(isMobile) return res.status(200).send("Your password has been changed successfully!");
+      return res.redirect("/student/profile");
     }
+    if(isMobile) return res.status(201).send("Your password is not correctly, if you forget your password, please contact to admin");
     return res.send(
       "Your password is not correctly, if you forget your password, please contact to admin"
     );
@@ -114,7 +92,6 @@ const changePassword = async (req, res, next) => {
 const getNews = async (req, res, next) => {
   try {
     const isMobile = req.header("Accept").includes("application/json");
-
     const { newsID } = req.params;
     const news = await News.findById(newsID);
     if (isMobile) return res.status(200).send(news);
@@ -129,7 +106,6 @@ const home = async (req, res, next) => {
     const isMobile = req.header("Accept").includes("application/json");
     const news = await News.find({});
     if (isMobile) return res.status(200).send(news);
-
     user = req.session.user;
     if (user && user.type === "Student") {
       const learning = await News.find({ type: "Learning" });
@@ -147,14 +123,16 @@ const home = async (req, res, next) => {
 const profile = async (req, res, next) => {
   try {
     const isMobile = req.header("Accept").includes("application/json");
-    if (isMobile) {
-      const {studentID} = req.query;
-      const student = await User.findById(studentID);
-      return res.status(200).send(student);
-    };
-    
-    const studentID = user._id;
+    var studentID;
+    if (user) {
+      studentID = user._id;
+    } else {
+      studentID = req.query.studentID;
+    }
     const student = await User.findById(studentID);
+    if (isMobile) {
+      return res.status(200).send(student);
+    }
     return res.render("student/profile", { student, user });
   } catch (error) {
     return res.send(error.message);
@@ -168,7 +146,7 @@ const registration = async (req, res, next) => {
       "subjects"
     );
     const data = semester.subjects;
-    if(isMobile) return res.status(200).send(data);
+    if (isMobile) return res.status(200).send(data);
     return res.render("student/registration", { data, user });
   } catch (error) {
     return res.send(error.message);
@@ -179,60 +157,15 @@ const registration = async (req, res, next) => {
 const schedule = async (req, res, next) => {
   try {
     const isMobile = req.header("Accept").includes("application/json");
-    if(isMobile){
-      const {userID} = req.query;
-      const userFound = await User.findById(userID);
-      const schedule = await Schedule.findById(userFound.schedule).populate(
-        "subjects.subject"
-      );
-      const data = [...schedule.subjects];
-      const scheduleList = [];
-      data.forEach((item) => {
-        const start = new Date(item.startDate);
-        const end = new Date(item.endDate);
-        var stringDate;
-        for (start; start <= end; start.setDate(start.getDate() + 1)) {
-          var date = start.getDate();
-          var month = start.getMonth() + 1;
-          var year = start.getFullYear();
-          var day = start.getDay() + 1;
-          stringDate =
-            (date < 10 ? "0" + date : date) +
-            "/" +
-            (month < 10 ? "0" + month : month) +
-            "/" +
-            year;
-          const subject = {
-            subjectID: item.subject.subjectID,
-            name: item.subject.name,
-            shift: item.shift,
-            block: item.block,
-            stringDate: stringDate,
-            date: start,
-          };
-          if (item.days === "Even") {
-            if (day % 2 == 0) {
-              scheduleList.push(subject);
-            }
-          } else if (item.days === "Odd") {
-            if (day % 2 != 0 && day != 1) {
-              scheduleList.push(subject);
-            }
-          }
-        }
-      });
-      scheduleList.sort((a, b) => {
-        var dateA = new Date(a.date).getTime();
-        var dateB = new Date(b.date).getTime();
-        if (dateA > dateB) return 1;
-        if (dateA < dateB) return -1;
-        return 0;
-      });
-      return res.status(200).send(scheduleList);
+
+    var scheduleID;
+    if (user) {
+      scheduleID = user.schedule;
+    } else {
+      const { userID } = req.query;
+      const student = await User.findById(userID);
+      scheduleID = student.schedule;
     }
-
-
-    const scheduleID = user.schedule;
     const schedule = await Schedule.findById(scheduleID).populate(
       "subjects.subject"
     );
@@ -280,7 +213,9 @@ const schedule = async (req, res, next) => {
       if (dateA < dateB) return -1;
       return 0;
     });
-
+    if (isMobile) {
+      return res.status(200).send(scheduleList);
+    }
     // Pagination
     const page = parseInt(req.query.page) || 1;
     const perPage = 10;
@@ -301,19 +236,21 @@ const schedule = async (req, res, next) => {
 const transcript = async (req, res, next) => {
   try {
     const isMobile = req.header("Accept").includes("application/json");
-    if (isMobile) {
+    var studentID;
+    if (user) {
+      studentID = user._id;
+    } else {
       const { userID } = req.query;
-      const transcript = await Transcript.findOne({ owner: userID }).populate({
-        path: "subjects.subject",
-      });
-      const data = [...transcript.subjects];
-      return res.status(200).send(data);
+      const student = await User.findById(userID);
+      studentID = student._id;
     }
-    const studentID = user._id;
     const transcript = await Transcript.findOne({ owner: studentID }).populate({
       path: "subjects.subject",
     });
-
+    if (isMobile) {
+      const data = [...transcript.subjects];
+      return res.status(200).send(data);
+    }
     return res.render("student/transcript", {
       transcript: transcript.subjects,
       user,
