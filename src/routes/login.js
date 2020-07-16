68af8b647d3ca7927987e8e5c07a7cd27c39a164
Trong Nhan <trongnhannguyen94@gmail.com>
2020-07-16 13:23:23 +0700

    16/07/2020

--- src/routes/login.js
+++ src/routes/login.js
@@ -6,16 +6,6 @@ const Subject = require("../model/Subject");
 // Get
 route.get("/", loginController.login);
 route.get("/logout", loginController.logout);
-// Training query
-route.get("/query", async (req, res) => {
-  try {
-    const { name } = req.query;
-    const subjects = await Subject.find({ name: {$regex: new RegExp(name), $options: 'i'}});
-    return res.send(subjects);
-  } catch (error) {
-    return res.send(error);
-  }
-});
 // Post
 route.post("/login", loginController.getUser);
 module.exports = route;
